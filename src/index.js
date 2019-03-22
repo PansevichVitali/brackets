035b7db6b491c7df5dcfcd2132f55a66cffcee71
PansevichVitali <egyptiain94@gmail.com>
2019-03-23 01:44:06 +0300

    style: change white-space

--- src/index.js
+++ src/index.js
@@ -2,23 +2,24 @@ module.exports = function check(str, bracketsConfig) {
   const openingBrackets = [];
   const closingBrackets = [];
   const strCheck = [];
-  for(let i = 0; i < bracketsConfig.length; i++) {
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
     openingBrackets.push(bracketsConfig[i][0]);
     closingBrackets.push(bracketsConfig[i][1]);
   }
 
-  for(let i = 0; i < str.length; i++) {
-    for(let j = 0; j < bracketsConfig.length; j++) {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
       if ((str[i] === closingBrackets[j]) && (strCheck.length === 0) && (str[i] !== openingBrackets[j]))
         return false;
-      else if((str[i] === closingBrackets[j]) && (strCheck[strCheck.length-1] === openingBrackets[j]))
+      else if ((str[i] === closingBrackets[j]) && (strCheck[strCheck.length-1] === openingBrackets[j]))
         strCheck.pop();
-      else if(str[i] === openingBrackets[j])
+      else if (str[i] === openingBrackets[j])
         strCheck.push(str[i]);
     }
   }
 
-  if(strCheck.length === 0) 
+  if (strCheck.length === 0) 
     return true;
   else 
     return false;
